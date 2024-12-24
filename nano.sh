27c36f619dfdbb7f8c90e7e810f897f11dbef708
Xsoft <127049334+NoltixOS@users.noreply.github.com>
2024-12-24 22:59:02 +0200

    Update and rename fet.sh to nano.sh

--- nano.sh
+++ nano.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-WM=${WAYLAND_DISPLAY:-$(xprop -id "$(xprop -root _NET_SUPPORTING_WM_CHECK | cut -d' ' -f5)" _NET_WM_NAME | cut -d'"' -f2)}
 echo -e "------------OS VER------------"
 grep -m1 -o '[^NAME=]' '/etc/os-release' | cut -d '"' -f 2 | tr -d '\012\015' && printf "\n"
 echo -e "------------KERNEL------------"
@@ -12,7 +10,3 @@ echo -e "----------CPU MODEL-----------"
 grep -m 1 'model name' /proc/cpuinfo
 echo -e "----------GPU MODEL-----------"
 lspci | grep -Em2 'VGA' | cut -c 24-5000
-echo -e "-----------RAM INFO-----------"
-egrep 'MemTotal|MemAvailable' /proc/meminfo
-echo -e "------------SHELL-------------"
-basename $SHELL
